db7d1cd0d4ffdf95472056ae050d0c53fdd6693d
Santiago <santiago_abregu_@hotmail.com>
2024-10-03 01:47:48 -0300

    Player & Zombie can attack

--- entities/Player/player.gd
+++ entities/Player/player.gd
@@ -6,6 +6,53 @@ class_name Player
 @export var FRICTION = 1500
 @export var MAX_SPEED = 450
 
+var health = 100
+var attack_range: float = 150.0
+var attack_damage: float = 25.0
+var inventory = ["Crowbar"]
+var current_target: Node2D = null
+
+
+func attack():
+	if current_target and "Crowbar" in inventory:
+		var distance = position.distance_to(current_target.position)
+		if distance <= attack_range:
+			current_target.take_damage(attack_damage)
+			print("Attacked zombi! Remaining HP: ", current_target.HEALTH_POINTS)
+		else:
+			print("Target too far!")
+	else:
+		print("No target in range or no Crowbar in inventory!")
+
+
+func _input(event):
+	if event.is_action_pressed("attack"):
+		attack()
+
+
+func add_to_inventory(item):
+	if inventory.size() < 12:
+		inventory.append(item)
+		print("Added to inventory: ", item)
+	else:
+		print("Inventory full!")
+
+
+func remove_from_inventory(item):
+	if item in inventory:
+		inventory.erase(item)
+		print("Removed from inventory: ", item)
+	else:
+		print("Item not in inventory!")
+
+
+func use_item(item):
+	if item in inventory:
+		print("Using item: ", item)
+		inventory.erase(item)
+	else:
+		print("Item not in inventory!")
+
 
 func _physics_process(delta: float) -> void:
 	move(delta)
@@ -30,3 +77,21 @@ func apply_friction(amount):
 func apply_movement(accel):
 	velocity += accel
 	velocity = velocity.limit_length(MAX_SPEED)
+
+
+func take_damage(amount):
+	if health > 0:
+		health -= amount
+		print("Nico health:", health)
+
+
+func _on_crowbar_area_body_entered(body: Node2D) -> void:
+	if body is Zombie:
+		current_target = body
+		# print("Zombie in range!")
+
+
+func _on_crowbar_area_body_exited(body: Node2D) -> void:
+	if body == current_target:
+		current_target = null
+		# print("Zombie out of range!")
--- entities/enemies/zombie.gd
+++ entities/enemies/zombie.gd
@@ -1,29 +1,71 @@
 extends CharacterBody2D
+class_name Zombie
 
-@export var HEALTH_POINTS: float  = 100.0 
+@export var HEALTH_POINTS: float = 100.0 
 @export var SPEED: float = 50.0
+@export var ATTACK_DAMAGE = 25
+@export var attack_interval: float = 1.0
 
 var raycast: RayCast2D
-
 var target: Node2D
+var target_in_attack_area: bool = false
+var attack_timer: Timer
 
 func _ready() -> void:
 	raycast = $RayCast2D
+	attack_timer = Timer.new()
+	attack_timer.one_shot = true
+	attack_timer.wait_time = attack_interval
+	attack_timer.connect("timeout", Callable(self, "_on_attack_timeout"))
+	add_child(attack_timer)
+
 
 func _physics_process(delta: float) -> void:
-	if(target):
+	if target:
 		raycast.target_position = raycast.to_local(target.global_transform.origin)
 		raycast.force_raycast_update()
 		
 		if raycast.is_colliding() && raycast.get_collider() == target:
 			position += (target.position - position).normalized() * SPEED * delta
-			move_and_collide(Vector2(0,0)) 
-			
+			move_and_collide(Vector2(0,0))
+
+			if target_in_attack_area and attack_timer.is_stopped():
+				attack_timer.start()  # Comienza el ataque si no está atacando
+
+
+func take_damage(amount: float) -> void:
+	HEALTH_POINTS -= amount
+	if HEALTH_POINTS <= 0:
+		queue_free()
+
+
+func attack(target):
+	if target is Player:
+		target.take_damage(ATTACK_DAMAGE)
+		# print("Zombie attacked! Player remaining HP: ", target.HEALTH_POINTS)
+
+
+func _on_attack_timeout() -> void:
+	if target_in_attack_area:
+		attack(target)
+		attack_timer.start()
+
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	target = body;
-	
 
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	target = null
+
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		target_in_attack_area = true
+		attack_timer.start()
+
+
+func _on_attack_area_body_exited(body: Node2D) -> void:
+	if body is Player:
+		target_in_attack_area = false
+		attack_timer.stop()

71b01bb7b2bfcb950c1886bcd725c46c57f4a482
Santiago <santiago_abregu_@hotmail.com>
2024-11-18 01:15:37 -0300

    Feat: Items Colition

--- entities/world/collectable_object.gd
+++ entities/world/collectable_object.gd
@@ -5,7 +5,9 @@ class_name CollectableObject
 @onready var spr: Sprite2D
 
 var player_in_collecting_area: bool = false
-var target: Node2D;
+var target: Node2D
+var original_material: Material
+var highlight_material: ShaderMaterial
 
 func _process(delta):
 	if player_in_collecting_area and Input.is_action_just_pressed("collect"):
@@ -17,7 +19,6 @@ func _on_body_entered(body):
 	if body is not Player: return;
 	player_in_collecting_area = true
 	target = body
-	
 
 
 func _on_body_exited(body):
